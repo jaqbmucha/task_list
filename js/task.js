dc34fe686061e35b594a7f7ac0c6f119d9c48fd5
Jakub Mucha <kuba@mucha.codes>
2021-05-12 18:35:41 +0200

    added simple task list viewing on JS

---
+++ js/task.js
@@ -0,0 +1,38 @@
+{
+    const tasks = [
+        {
+            taskName: "Pouczyć się kodu JavaScript",
+            done: true,
+        },
+        {
+            taskName: "Zrobić zadanie domowe z kursu YouCode.pl",
+            done: false,
+        },
+    ];
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <div class="taskList__list js-task">
+            <div class="listItem">
+            <div class="listItem__taskButton"><button class="listItem__Button listItem__Button--check">${task.done ? "&#10004;" : ""}</button>
+            </div>
+            <div class="listItem__taskName">
+            <p${task.done ? " class=\"listItem__taskDone\"" : ""}>${task.taskName}</p>
+            </div>
+            <div class="listItem__taskButton">
+                <button class="listItem__Button listItem__Button--delete">&#128465;</button>
+            </div>
+            </div>
+            </div>
+            `;
+        }
+        document.querySelector(".js-task").innerHTML = htmlString;
+
+    };
+    const init = () => {
+        render();
+    };
+    init();
+}
